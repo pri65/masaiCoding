fbdb6f1408a899e7e683db84a2eb89e846f3ab49
pri65 <priyankasoniii65@gmail.com>
2022-05-22 22:22:29 +0530

    append

--- unit_3/coding/day5 psc student management system/dashboard.js
+++ unit_3/coding/day5 psc student management system/dashboard.js
@@ -2,23 +2,48 @@ function append(){
     let data=JSON.parse(localStorage.getItem("Student")) || [];
 let container=document.getElementById("container");
 
-    data.forEach(function(el){
-        let div=document.createElement('div');
-    let p=document.createElement('p');
-    p.innerText=el.names;
-    let q=document.createElement('q');
-    q.innerText=el.course;
-    let r=document.createElement('r');
-    r.innerText=el.unit;
-    let img=document.createElement('img');
+    data.forEach(function(el,index){
+    let div=document.createElement("div");
+    let img=document.createElement("img");
     img.src=el.img;
-    let s=document.createElement('s');
+    let p=document.createElement("p");
+    p.innerText=el.name;
+    let q=document.createElement("q");
+    q.innerText=el.course;
+    let r=document.createElement("r");
+    r.innerText=el.unit;
+    let s=document.createElement("s");
     s.innerText=el.batch;
+    let btn=document.createElement("button");
+    btn.innerText=remove;
+    btn.setAttribute("click",function(){
+        remove(index);
+    });
 
-    div.append(p,q,r,img,s);
+    div.append(img,p,q,r,s,btn);
     container.append(div);
 });
-    
+}
+append();
+
+function remove(index){
+    let data=JSON.parse(localStorage.getItem("Student")) || [];
+
+    let newData=data.filter(function(el,i){
+        if(i===index){
+        let trash=JSON.parse(localStorage.getItem("trash")) || [];
+        trash.push(el);
+        localStorage.setItem('trash',JSON.stringify(trash));
+        } else {
+            return i !== index;
+        }
+    });
+
+    localStorage.setItem("students",JSON.stringify(newData));
+    append();
+
+
+    console.log(newData);
 }
 
-append();
+
