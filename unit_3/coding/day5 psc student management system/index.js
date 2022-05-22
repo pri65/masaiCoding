41d08dbb94450aeade4118f1b1100f440ebb15f5
pri65 <priyankasoniii65@gmail.com>
2022-05-22 19:35:16 +0530

    values created

--- unit_3/coding/day5 psc student management system/index.js
+++ unit_3/coding/day5 psc student management system/index.js
@@ -2,5 +2,11 @@
 
 function storeData(){
     //this function will collect all the data from input box and create an object out of it and store it to the local storage
-    
+    let form=document.getElementById("students_data");
+    let name=form.name.value;  
+    //form.id.value
+    let course=form.course.value;
+    let unit=form.unit.value;
+    let image=form.image.value;
+    let batch=form.batch.value;
 }
