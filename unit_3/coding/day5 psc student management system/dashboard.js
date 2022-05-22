90d4284be696a49c62200d4badedf5a0f6cfea1c
pri65 <priyankasoniii65@gmail.com>
2022-05-22 22:00:50 +0530

    info

--- unit_3/coding/day5 psc student management system/dashboard.js
+++ unit_3/coding/day5 psc student management system/dashboard.js
@@ -2,9 +2,20 @@ function append(){
     let data=JSON.parse(localStorage.getItem("Student")) || [];
 
     data.forEach(function(el){
-    console.log(el);
+        let div=document.createElement('div');
+    let p=document.createElement('p');
+    p.innerText=el.names;
+    let q=document.createElement('q');
+    q.innerText=el.course;
+    let r=document.createElement('r');
+    r.innerText=el.unit;
+    let img=document.createElement('img');
+    img.src=el.img;
+    let s=document.createElement('s');
+    s.innerText=el.batch;
+
 });
-    let div=document.createElement('div');
+    
 }
 
 append();
