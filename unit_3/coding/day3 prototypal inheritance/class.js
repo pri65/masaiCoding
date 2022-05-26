80478fd964468a14e15b19e8eeaaca3aeacdbc95
pri65 <priyankasoniii65@gmail.com>
2022-05-26 22:50:21 +0530

    push

--- unit_3/coding/day3 prototypal inheritance/class.js
+++ unit_3/coding/day3 prototypal inheritance/class.js
@@ -120,18 +120,18 @@ function myArray() {
   }
 }
 
-// let arr3 = new myArray(1, 2, 3, 4, 5);
+let arr3 = new myArray(1, 2, 3, 4, 5);
 
-// // console.log(Object.values(arr3));
+// console.log(Object.values(arr3));
 
-// arr = [1,2,3,4];
-//length = 4;
-// push(6) ---> index 4
+arr = [1,2,3,4];
+length = 4;
+push(6) ---> index 4
 
-// myArray.prototype.पुश = function (value) {
-//   this[this.length] = value;
-//   this.length++;
-// };
+myArray.prototype.push = function (value) {
+  this[this.length] = value;
+  this.length++;
+};
 
 // myArray.prototype.हटाओ = function () {
 //   delete this[this.length - 1];
