b3f65e7722ea9167d6ccdffd099a758165584664
pri65 <priyankasoniii65@gmail.com>
2022-05-26 22:40:34 +0530

    var

--- unit_3/coding/day3 prototypal inheritance/class.js
+++ unit_3/coding/day3 prototypal inheritance/class.js
@@ -9,21 +9,21 @@
 
 // Cat family :- Tiger, lion, panther, leopard, cheetah
 
-// const cat = {
-//   name: "cat",
-//   legs: 4,
-//   fur: true,
-//   tail: true,
-//   type: "animal",
-// };
+const cat = {
+  name: "cat",
+  legs: 4,
+  fur: true,
+  tail: true,
+  type: "animal",
+};
 
-// const tiger = {
-//   name: "tiger",
-//   legs: 4,
-//   fur: true,
-//   tail: true,
-//   type: "animal",
-// };
+const tiger = {
+  name: "tiger",
+  legs: 4,
+  fur: true,
+  tail: true,
+  type: "animal",
+};
 
 // const lion = {
 //   name: "lion",
