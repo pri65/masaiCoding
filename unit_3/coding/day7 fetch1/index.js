16f7d4c0b22abf2a04603076e20dafff47813a08
pri65 <priyankasoniii65@gmail.com>
2022-05-27 23:14:04 +0530

    new

--- unit_3/coding/day7 fetch1/index.js
+++ unit_3/coding/day7 fetch1/index.js
@@ -8,17 +8,17 @@
 
 */
 
-// let x = fetch(); // It will take time
-// console.log(x);
+let x = fetch(); // It will take time
+console.log(x);
 
-// Youtube, Google map, facebook, twitter
+Youtube, Google map, facebook, twitter
 
-//const url = "https://reqres.in/api/users"; // Destination
+const url = "https://reqres.in/api/users"; // Destination
 
-// fetch(url)
-//   .then(function (res) {
-//     // Readable + Stream( data in chunks )
-//     // Collect the stream
+fetch(url)
+  .then(function (res) {
+    // Readable + Stream( data in chunks )
+    // Collect the stream
 //     return res.json(); // res.collect()
 //   })
 //   .then(function (res) {
