a8a22ef0dcb600bee13a05d1a597a498ce5ac6f0
pri65 <priyankasoniii65@gmail.com>
2022-05-29 15:34:39 +0530

    temp details

--- unit_3/coding/day8 fetch2/weather app/main.js
+++ unit_3/coding/day8 fetch2/weather app/main.js
@@ -4,9 +4,9 @@ const api_key="fdd17e86ec35dbce60c931f62d9ec331"
 async function getData(){
     let city=document.getElementById("city").value;
     let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-//url is our destination
+    //url is our destination
     //now lets check wthr we are getting correct url or not
-   // console.log(url);
+    // console.log(url);
 let res=await fetch(url);
 let data=await res.json();
 append(data);
@@ -15,17 +15,18 @@ append(data);
 
 function append(data){
     let container=document.getElementById("container");
+    container.innerHTML=null;
         let h3=document.createElement("h3");
-        h3.innerText=el.name;
+        h3.innerText=data.name;
 
         let p1=document.createElement("p")
-        p.innerText=`Current temp: $(data.main.temp)`;
+        p1.innerText=`Current temp: ${data.main.temp}`;
 
         let p2=document.createElement("p");
-        p.innerText=`Max temp: $(data.main.temp_max)`;
+        p2.innerText=`Max temp: ${data.main.temp_max}`;
 
         let p3=document.createElement("p");
-        p.innerText=`Min temp: $(data.main.temp_min)`;
+        p3.innerText=`Min temp: ${data.main.temp_min}`;
 
         container.append(h3,p1,p2,p3);
 }
