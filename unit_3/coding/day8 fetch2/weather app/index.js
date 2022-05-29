7c41ada5abfd01b4898877bf86209e7d57cc6111
pri65 <priyankasoniii65@gmail.com>
2022-05-29 14:03:49 +0530

    new

--- unit_3/coding/day8 fetch2/weather app/index.js
+++ unit_3/coding/day8 fetch2/weather app/index.js
@@ -1,8 +1,13 @@
 const api_key="fdd17e86ec35dbce60c931f62d9ec331"
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-function getData(){
+async function getData(){
     let city=document.getElementById("city").value;
     let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
+//url is our destination
+    //now lets check wthr we are getting correct url or not
+   // console.log(url);
+let res=await fetch(url);
+let data=res.json();
+   
 }
