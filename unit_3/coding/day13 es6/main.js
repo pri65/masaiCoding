29ec0bf2ca365dfb6af97b7a6e66591abbf7a2bc
pri65 <priyankasoniii65@gmail.com>
2022-06-01 15:35:02 +0530

    new

--- unit_3/coding/day13 es6/main.js
+++ unit_3/coding/day13 es6/main.js
@@ -1,9 +1,18 @@
 //why cf
 //to avoid DRY
 
-{
-    name:"",
-    print:function(){
-        console.log(this.name);
-    }
+// {
+//     name:"",
+//     print:function(){
+//         console.log(this.name);
+//     }
+// }
+
+//traditional way
+
+function Person(n) {
+    this.name=n;
+}
+Person.prototype.print=function(){
+    
 }
