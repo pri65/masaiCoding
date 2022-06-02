35122a7f87bd57d7787523e6e1cb1bc3ca4c0532
pri65 <priyankasoniii65@gmail.com>
2022-06-02 21:43:17 +0530

    new

--- unit_3/coding/day12 es6/main.js
+++ unit_3/coding/day12 es6/main.js
@@ -5,4 +5,7 @@
 
 //https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=thor&key=[YOUR_API_KEY
 
+//SPREAD
+//at first everything is in packet then we spread it
+//syntax for spread is ...
 
