8d1d84404452f0a7905e9bfc7dca27adce6f1fc5
pri65 <priyankasoniii65@gmail.com>
2022-06-02 23:40:29 +0530

    new

--- unit_3/coding/day12 es6/class/main.js
+++ unit_3/coding/day12 es6/class/main.js
@@ -65,10 +65,11 @@ let append=()=>{
 }
 
 
-{/* <iframe width="560" height="315"
+/* <iframe width="560" height="315"
  src="https://www.youtube.com/embed/zdp0zrpKzIE" 
  title="YouTube video player"
   frameborder="0" 
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+   picture-in-picture" 
   allowfullscreen>
-  </iframe> */}
+  </iframe> */
