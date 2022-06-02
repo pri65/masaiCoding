6d4724c413c8f414dfd2d80172cbc7a2a2866d02
pri65 <priyankasoniii65@gmail.com>
2022-06-02 22:07:01 +0530

    new

--- unit_3/coding/day12 es6/main.js
+++ unit_3/coding/day12 es6/main.js
@@ -1,10 +1,3 @@
-//api key=AIzaSyBa-WI4hKi9npmNiFi2KrGPytuPgUnaA5I
-//youtube
-//url
-//fetch()
-
-//https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=thor&key=[YOUR_API_KEY
-
 //SPREAD
 //at first everything is in packet then we spread it
 //syntax for spread is ...
@@ -26,3 +19,11 @@
 // ["Box2","Box3","Box4"]
 //NOTE:-> we cant write fun(...b,a)
 //we cant also write;-> fun(a,b,...c)
+
+
+//api key=AIzaSyBa-WI4hKi9npmNiFi2KrGPytuPgUnaA5I
+//youtube
+//url
+//fetch()
+
+//https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=thor&key=[YOUR_API_KEY
