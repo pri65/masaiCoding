4b77588a9d11b0259c67a7f883258e526c141639
pri65 <priyankasoniii65@gmail.com>
2022-06-03 15:29:39 +0530

    new

--- unit_3/coding/day15 lld/main.js
+++ unit_3/coding/day15 lld/main.js
@@ -3,3 +3,15 @@
 //road:- head of village(lld)
 //nh:- nititin gatkari(hld)
 //parking system
+
+class vehicle{
+    constructor(type,regNumber,color) {
+        this.type=type;
+        this.regNumber=regNumber;
+        this.color=color;
+    }
+
+    get type() {
+        return this.type;
+    }
+}
