0abdc6e750be1e0227092a7fae4619dc253e9baf
pri65 <priyankasoniii65@gmail.com>
2022-06-04 12:04:00 +0530

    new

--- unit_3/coding/day13 es6/main.js
+++ unit_3/coding/day13 es6/main.js
@@ -9,5 +9,14 @@
 //     }
 
 //NOW MAKE A CF WHICH WILL MAKE ABOVE KIND OF OBJECT
+//1. TRADITIONAL WAY
+// function Person(n) {
+//   this.name = n;
+// }
+// Person.prototype.print = function () {
+//   console.log(this.name);
+// };
+// let p1 = new Person("Anu");
+// console.log(p1);
 
 
