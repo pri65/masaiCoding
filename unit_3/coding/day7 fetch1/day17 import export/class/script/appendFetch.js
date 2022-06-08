daf6f13f47ce437b49595db5ae0d3e5aadb4a1ae
pri65 <priyankasoniii65@gmail.com>
2022-06-08 21:08:26 +0530

    new

--- unit_3/coding/day7 fetch1/day17 import export/class/script/appendFetch.js
+++ unit_3/coding/day7 fetch1/day17 import export/class/script/appendFetch.js
@@ -1,6 +1,21 @@
 let getData=async(url) =>{
+    //this function will fetch the data and return the data.
     let res=await fetch(url);
     let data=await res.JSON();
    return data; 
    };
-   
+
+   let append=(data) =>{
+    let container=document.getElementById("container");
+
+   data.forEach((el)=>{
+    let div=document.createElement("div")
+    let p=document.createElement("p")
+    p.innerText=el.title;
+    let img=document.createElement("img")
+    img.src=el.image;
+
+div.append(img,p);
+container.append(div);
+});
+};
