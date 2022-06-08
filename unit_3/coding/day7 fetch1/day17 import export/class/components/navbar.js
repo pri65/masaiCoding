9e99b688940d45c716e6b910d8d38fc916c33d0b
pri65 <priyankasoniii65@gmail.com>
2022-06-08 11:27:51 +0530

    new

--- unit_3/coding/day7 fetch1/day17 import export/class/components/navbar.js
+++ unit_3/coding/day7 fetch1/day17 import export/class/components/navbar.js
@@ -2,3 +2,27 @@
 //using a function we can return anything
 //so first make a function
 //then we want to return the html tag
+//to return something we need back ticks``
+
+
+function navbar() {
+    return `
+        <h3>
+            <a href="/">Home</a>
+        </h3>
+        <div id="options">
+            <h3>
+                <a href="electronics.html">Electronics</a>
+            </h3>
+            <h3>
+                <a href="jewellery.html">Jewellery</a>
+            </h3>
+            <h3>
+                <a href="#">Login</a>
+            </h3>
+            <h3>
+                <a href="#">Signup</a>
+            </h3>
+        </div>
+    `
+}
