d4132c8de14fb27d117e90b280fb1259aa42622d
pri65 <priyankasoniii65@gmail.com>
2022-06-12 20:32:17 +0530

    js

---
+++ unit_3/coding/day17 import export/assign/food app 3/scripts/main.js
@@ -0,0 +1,6 @@
+import navbar from '../components/navbar.js';
+let navbarBox = document.querySelector('#navbar');
+navbarBox.innerHTML = navbar();
+let user = JSON.parse(localStorage.getItem('signinData'));
+let name = document.querySelector('#name');
+name.innerText = user.username;
