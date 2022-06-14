7e81865adb5331de733f25fa907d926af7d3992b
pri65 <priyankasoniii65@gmail.com>
2022-06-14 13:22:52 +0530

    new

---
+++ unit_3/coding/day19 psc webpack/components/navbar.js
@@ -0,0 +1,16 @@
+let navbar = () => {
+    return `
+    <div id="search_bar">
+            <input type="text" id="query">
+        </div>
+        <div id="categories">
+            <h3>Nature</h3>
+            <h3>Cars</h3>
+            <h3>Music</h3>
+            <h3>Cartoons</h3>
+            <h3>Technology</h3>
+            <h3>Marvel</h3>
+    </div> `
+}
+
+export { navbar };
---
+++ unit_3/coding/day19 psc webpack/scripts/index.js
@@ -0,0 +1,5 @@
+//link:- https://api.unsplash.com/search/photos/?query=${value}&per_page=20&client_id=s9-74SupucxGrmswXVJ0dQtc3CC03MfzWfvKlk7Ys0g
+
+import { navbar } from "../components/navbar.js"; 
+let n=document.getElementById("navbar")
+n.innerHTML=navbar();
