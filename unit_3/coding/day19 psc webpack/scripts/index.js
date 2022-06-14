dc037487a31844c5953f1a7324188915a2732426
pri65 <priyankasoniii65@gmail.com>
2022-06-14 15:19:14 +0530

    new

--- unit_3/coding/day19 psc webpack/scripts/index.js
+++ unit_3/coding/day19 psc webpack/scripts/index.js
@@ -1,5 +1,21 @@
 //link:- https://api.unsplash.com/search/photos/?query=${value}&per_page=20&client_id=s9-74SupucxGrmswXVJ0dQtc3CC03MfzWfvKlk7Ys0g
 
+const API="s9-74SupucxGrmswXVJ0dQtc3CC03MfzWfvKlk7Ys0g";
+
 import { navbar } from "../components/navbar.js"; 
 let n=document.getElementById("navbar")
 n.innerHTML=navbar();
+
+
+let searchImages = async () =>{
+    let query=document.getElementById("query").value;
+   try {
+    let res= await fetch {
+        'https://api.unsplash.com/search/photos/?query=${value}&per_page=20&client_id=${API}'
+    };
+let data=await res.JSON();
+   } catch (err) {
+    console.log(err);
+   }
+
+};
