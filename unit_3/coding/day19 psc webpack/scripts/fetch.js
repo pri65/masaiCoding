299a3f4113abb3d4e994aefc8be95d8da1548e04
pri65 <priyankasoniii65@gmail.com>
2022-06-15 20:05:07 +0530

    new

--- unit_3/coding/day19 psc webpack/scripts/fetch.js
+++ unit_3/coding/day19 psc webpack/scripts/fetch.js
@@ -1,13 +1,30 @@
-let searchImages = (API,value) =>{
+let searchImages = async (API,value) => {
     try {
-        let res= await fetch{
-            'https://api.unsplash.com/search/photos/?query=${value}&per_page=20&client_id=${API}'
-        }
+        let res= await fetch(
+            `https://api.unsplash.com/search/photos/?query=${value}&per_page=20&client_id=${API}`
+        );
     let data=await res.JSON();
     return data;
        } catch (err) {
         console.log(err);
        }
-}
+};
 
-export { searchImages }
+let append = (data) => {
+    let container = document.getElementById("container");
+    container.innerHTML = null;
+    data.forEach((el) => {
+      let div = document.createElement("div");
+      div.setAttribute("class", "image");
+      let img = document.createElement("img");
+      let h3 = document.createElement("h3");
+  
+      img.src = el.urls.small;
+      h3.innerText = el.alt_description;
+  
+      div.append(img, h3);
+      container.append(div);
+    });
+  };
+
+export { searchImages,append };
--- unit_3/coding/day19 psc webpack/scripts/index.js
+++ unit_3/coding/day19 psc webpack/scripts/index.js
@@ -6,13 +6,16 @@ import { navbar } from "../components/navbar.js";
 let n=document.getElementById("navbar")
 n.innerHTML=navbar();
 
-import { searchImages } from "./fetch.js";
+import { searchImages,append } from "./fetch.js";
 
 let search = (e) => {
-    if(e.key=== "Enter") {
+    if(e.key === "Enter") {
         let value=document.getElementById("query").value;
-        searchImages(API,value );
+       searchImages(API,value ).then((data) => {
         console.log(data);
+        let container=document.getElementById("container");
+        append(data.results,container);
+        });
     }
 };
 
