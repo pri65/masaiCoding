2756c5bda2e1235e1be54cfeb64166aeac33f25e
pri65 <priyankasoniii65@gmail.com>
2022-06-27 17:54:08 +0530

    new

--- unit4/day2/login-app/src/App.js
+++ unit4/day2/login-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import LoginForm from './Components/LoginForm';
 
 function App() {
   return (
     <div>
-    
+    <LoginForm />
     </div>
   )
 }
---
+++ unit4/day2/login-app/src/Components/LoginForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function LoginForm() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default LoginForm;
