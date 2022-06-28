faa0884ff39ce46c061ba91f8a63316e43839cb2
pri65 <priyankasoniii65@gmail.com>
2022-06-28 12:28:01 +0530

    new

--- unit4/day2/counter/src/App.js
+++ unit4/day2/counter/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div>
-
+      <counter />
     </div>
   );
 }
