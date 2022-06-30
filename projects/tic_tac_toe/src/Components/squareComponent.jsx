30853969e0157bd6eb1e4789b33068fb0f7fad37
pri65 <priyankasoniii65@gmail.com>
2022-06-30 18:23:16 +0530

    new

---
+++ projects/tic_tac_toe/src/Components/squareComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function squareComponent() {
+  return (
+    <div className="app-header">
+    <p className="heading-text">Tic Tac Toe</p>
+    <div className="row jc-center">
+     X
+    </div>
+    <div className="row jc-center">
+     X
+    </div>
+    <div className="row jc-center">
+     X
+    </div>
+    </div>
+  )
+  }
+
+export default squareComponent;
