b5edc40aae14c0874f07079a0ca91b085aac1599
pri65 <priyankasoniii65@gmail.com>
2022-06-30 14:12:37 +0530

    new

--- unit4/day3/todo/src/Components/ToDo.jsx
+++ unit4/day3/todo/src/Components/ToDo.jsx
@@ -3,13 +3,21 @@ import React, {useState} from 'react'
 function ToDo() {
     const [text, setText] = useState("")
     const [todo, setTodo] = useState([])
-    const handleClick = (e) => {
-        useState(e.target.value)
+    const handleClick = () => {
+       setTodo([...todo,text])
     }
+  const Change = (e) => {
+    setText(e.target.value);
+  }
   return (
     <div>
-        <input type="text" placeholder='type'  />
-        <button onClick={handleClick(e)}>Add</button>
+        <input onChange={(e) => {Change(e)}} type="text" placeholder='type'  />
+        <button onClick={handleClick}>Add</button>
+        <ul>
+          <li>
+          {todo}
+          </li>
+        </ul>
     </div>
   )
 }
