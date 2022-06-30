e15371c9c58cf5530340b3679b9690ee08133978
pri65 <priyankasoniii65@gmail.com>
2022-06-30 18:16:07 +0530

    new

--- projects/tic_tac_toe/src/App.js
+++ projects/tic_tac_toe/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app-header">
-     
+     <p className="heading-text">Tic Tac Toe</p>
     </div>
   );
 }
