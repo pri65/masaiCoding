05817eb4633074b76f5f0dcef1e0962104602608
pri65 <priyankasoniii65@gmail.com>
2022-06-30 18:18:29 +0530

    new

--- projects/tic_tac_toe/src/App.js
+++ projects/tic_tac_toe/src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="app-header">
      <p className="heading-text">Tic Tac Toe</p>
+     <div>
+      X
+     </div>
+     <div>
+      X
+     </div>
+     <div>
+      X
+     </div>
     </div>
   );
 }
