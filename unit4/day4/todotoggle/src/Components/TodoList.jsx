37d49a03a4e6923064a46cf84e5fce76cf56c98e
pri65 <priyankasoniii65@gmail.com>
2022-07-05 18:42:28 +0530

    new

--- unit4/day4/todotoggle/src/Components/TodoList.jsx
+++ unit4/day4/todotoggle/src/Components/TodoList.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-function TodoList() {
+function TodoList({
+    data,
+    handleToggle
+}) {
   return (
     <div>
-        
+        {
+            data.map(item=>
+                <TodoItem 
+                key={item.id}
+                id={item.id}
+                title={item.title} 
+                status={item.status} 
+                />
+                )
+        }
     </div>
   )
 }
 
-export default TodoList
+export default TodoList;
