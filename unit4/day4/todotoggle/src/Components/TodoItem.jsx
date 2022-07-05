8554b687c127b15b648a8b04c0582439a3612ea1
pri65 <priyankasoniii65@gmail.com>
2022-07-05 17:20:14 +0530

    new

--- unit4/day4/todotoggle/src/Components/TodoItem.jsx
+++ unit4/day4/todotoggle/src/Components/TodoItem.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function TodoItem() {
   return (
-    <div>
-        
+    <div 
+    style={{ 
+    padding: '1rem', 
+    border:"1px solid tomato", 
+    borderRadius:"1rem",
+    display:"flex",
+    gap:"1rem"
+    }}>
+
     </div>
   )
 }
