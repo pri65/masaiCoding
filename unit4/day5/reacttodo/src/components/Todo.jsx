b734da751274a5abf7d6da27f52bcb867fc58c0c
pri65 <priyankasoniii65@gmail.com>
2022-07-08 23:31:20 +0530

    new

--- unit4/day5/reacttodo/src/components/Todo.jsx
+++ unit4/day5/reacttodo/src/components/Todo.jsx
@@ -4,24 +4,28 @@ function Todo() {
     const [data, setData] = useState([]);
     const getTodos = async () => {
       try{
-        let data = await fetch("https://jsonplaceholder.typicode.com/todos")
+        let data = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10`)
         data=await data.json();
         console.log(data)
+        setData(data);
       }
       catch(err) {
-
+        console.log(err)
       }
     }
     
   return (
     <div>
         <h1>Todos</h1>
-        <button onClick={getTodos}>Fetch</button>
+        <button onClick={getTodos}>FETCH</button>
         <div>
           {
             data.map(todo=>
-              <div key={todo.id}>
-                {todo.title}
+              <div style={{display:"flex", gap:"1rem", margin:"0.5rem"}} 
+              key={todo.id}>
+                <div>{todo.id}</div>
+                <div>{todo.title}</div>
+                <div>{todo.status}</div>
                 </div>
                 )
           }
