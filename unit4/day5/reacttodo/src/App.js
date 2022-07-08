4fcbc8b6dab7b397b3b85032f67d3d8fecc80797
pri65 <priyankasoniii65@gmail.com>
2022-07-08 23:23:45 +0530

    new

--- unit4/day5/reacttodo/src/App.js
+++ unit4/day5/reacttodo/src/App.js
@@ -2,9 +2,10 @@
 import './App.css';
 import Todo from './components/Todo';
 
+
 function App() {
   return (
-    <div >
+    <div className='App'>
       <Todo />
     </div>
   );
--- unit4/day5/reacttodo/src/components/Todo.jsx
+++ unit4/day5/reacttodo/src/components/Todo.jsx
@@ -1,14 +1,31 @@
 import React, {useState,useEffect} from 'react'
 
 function Todo() {
-    const [todo, setTodo] = useState([]);
-    useEffect(() => {
-      
-    }, [])
+    const [data, setData] = useState([]);
+    const getTodos = async () => {
+      try{
+        let data = await fetch("https://jsonplaceholder.typicode.com/todos")
+        data=await data.json();
+        console.log(data)
+      }
+      catch(err) {
+
+      }
+    }
     
   return (
     <div>
-
+        <h1>Todos</h1>
+        <button onClick={getTodos}>Fetch</button>
+        <div>
+          {
+            data.map(todo=>
+              <div key={todo.id}>
+                {todo.title}
+                </div>
+                )
+          }
+        </div>
     </div>
   )
 }
