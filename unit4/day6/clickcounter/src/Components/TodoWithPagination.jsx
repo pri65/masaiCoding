8dc23fd9af6fe3b639eaa2a56203835623281791
pri65 <priyankasoniii65@gmail.com>
2022-07-09 20:42:45 +0530

    new

--- unit4/day6/clickcounter/src/Components/TodoWithPagination.jsx
+++ unit4/day6/clickcounter/src/Components/TodoWithPagination.jsx
@@ -4,7 +4,7 @@ function TodoWithPAgination(){
     const [data,setData]=useState([]);
     const getTodods = async () => {
         try{
-            let data=await fetch(`https://jsonplaceholder.typicode.com/todos?`)
+            let data=await fetch(`https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10`)
             data=await data.json();
             console.log(data)
             setData(data);
@@ -17,4 +17,25 @@ function TodoWithPAgination(){
     useEffect(()=>{
         getTodods()
     },[])
+
+    
+    return (
+        <div>
+            <h1>Todos</h1>
+            <div>
+              {
+                data.map(todo=>
+                  <div style={{display:"flex", gap:"1rem", margin:"0.5rem"}} 
+                  key={todo.id}>
+                    <div>{todo.id}</div>
+                    <div>{todo.title}</div>
+                    <div>{todo.status}</div>
+                    </div>
+                    )
+              }
+            </div>
+        </div>
+      )
 }
+
+export default TodoWithPAgination;
