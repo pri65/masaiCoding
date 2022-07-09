480b3e93afb864afb5b4e8049ab1b09f8ea60599
pri65 <priyankasoniii65@gmail.com>
2022-07-09 22:11:10 +0530

    new

--- unit4/day6/clickcounter/src/App.js
+++ unit4/day6/clickcounter/src/App.js
@@ -1,18 +1,15 @@
 
 import './App.css';
-import CartContainer from './Components/CartContainer';
-import Todo from './Components/Todo';
+// import CartContainer from './Components/CartContainer';
 import AfterRender from './Components/useEffectsExamples/AfterRender';
-import NestedComponent from './Components/useEffectsExamples/NestedComponent';
-import TodoWithPagination from './Components/TodoWithPagination';
+// import NestedComponent from './Components/useEffectsExamples/NestedComponent'
 
 function App() {
   return (
     <div className="App">
-      {/* <Todo /> */}
-      {/* <AfterRender /> */}
-     {/* <NestedComponent /> */}
-     <TodoWithPagination />
+      <AfterRender />
+     {/* <NestedComponent />
+     <CartContainer /> */}
     </div>
   );
 }
