f05891648d9e4beddd6b2cb6c3bb61505c12b041
pri65 <priyankasoniii65@gmail.com>
2022-07-09 22:47:53 +0530

    new

--- unit4/day7/class/src/Components/ParentToChild.jsx
+++ unit4/day7/class/src/Components/ParentToChild.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+const Child = ({ value }) => <h3>Count is:{value}</h3>;
 
 function ParentToChild() {
+const [count, setCount] = React.useState(0);
   return (
-    <div>ParentToChild</div>
+    <div>
+    <h1>Parent To Child</h1>
+    <Child value={count} />
+    <button onClick={() => setCount(count-1)}>-</button>   
+    <button onClick={() => setCount(count+1)}>+</button>   
+    </div>
   )
 }
 
-export default ParentToChild
+export {ParentToChild};
