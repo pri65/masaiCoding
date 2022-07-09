12c556aa8f9ff9419034bd5bd16377e21d59b45b
pri65 <priyankasoniii65@gmail.com>
2022-07-09 11:55:18 +0530

    new

--- unit4/day6/clickcounter/src/App.js
+++ unit4/day6/clickcounter/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import CartContainer from './Components/CartContainer';
 import Todo from './Components/Todo';
+import AfterRenderAlways from './Components/useEffectsExamples/AfterRender';
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      {/* <Todo /> */}
+      <AfterRenderAlways />
     </div>
   );
 }
--- unit4/day6/clickcounter/src/Components/useEffectsExamples/AfterRender.jsx
+++ unit4/day6/clickcounter/src/Components/useEffectsExamples/AfterRender.jsx
@@ -4,6 +4,7 @@ function AfterRenderAlways() {
     const [count, setCount]=useState(0);
     return(
         <div>
+            <h1>Count:{count}</h1>
             <button onClick={()=>setCount(count+1)}>INCREMENT</button>
         </div>
     )
