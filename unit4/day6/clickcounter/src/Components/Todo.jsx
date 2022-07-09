aa1983ee0d83d23656be8c8e863b983a418314ee
pri65 <priyankasoniii65@gmail.com>
2022-07-09 11:39:04 +0530

    new

---
+++ unit4/day6/clickcounter/src/Components/Todo.jsx
@@ -0,0 +1,42 @@
+import React, {useState,useEffect} from 'react'
+
+function Todo() {
+    const [data, setData] = useState([]);
+    const getTodos = async () => {
+      try{
+        let data = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10`)
+        data=await data.json();
+        console.log(data)
+        setData(data);
+        alert("Success")
+      }
+      catch(err) {
+        console.log(err)
+      }
+    }
+
+    useEffect(() =>{
+      alert("Making Requests")
+      getTodos()
+    },[])
+    
+  return (
+    <div>
+        <h1>Todos</h1>
+        <div>
+          {
+            data.map(todo=>
+              <div style={{display:"flex", gap:"1rem", margin:"0.5rem"}} 
+              key={todo.id}>
+                <div>{todo.id}</div>
+                <div>{todo.title}</div>
+                <div>{todo.status}</div>
+                </div>
+                )
+          }
+        </div>
+    </div>
+  )
+}
+
+export default Todo
