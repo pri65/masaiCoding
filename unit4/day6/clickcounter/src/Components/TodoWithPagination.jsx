147c5925840623aa63ee5d06b553164cc92e1015
pri65 <priyankasoniii65@gmail.com>
2022-07-09 20:53:39 +0530

    new

--- unit4/day6/clickcounter/src/Components/TodoWithPagination.jsx
+++ unit4/day6/clickcounter/src/Components/TodoWithPagination.jsx
@@ -2,6 +2,7 @@ import { useState,useEffect } from "react";
 
 function TodoWithPAgination(){
     const [data,setData]=useState([]);
+    const [page,setPage]=useState(1);
     const getTodods = async () => {
         try{
             let data=await fetch(`https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10`)
@@ -22,6 +23,9 @@ function TodoWithPAgination(){
     return (
         <div>
             <h1>Todos</h1>
+            <button onClick={()=>setPage(page=>page-1)} disabled={page===1}>PREV</button>
+            <span style={{padding:"0.5rem"}}>{page}</span>
+            <button onClick={()=>setPage(page=>page+1)}>NEXT</button>
             <div>
               {
                 data.map(todo=>
@@ -29,7 +33,7 @@ function TodoWithPAgination(){
                   key={todo.id}>
                     <div>{todo.id}</div>
                     <div>{todo.title}</div>
-                    <div>{todo.status}</div>
+                    <div>{todo.status}"Done":"Not Done"</div>
                     </div>
                     )
               }
