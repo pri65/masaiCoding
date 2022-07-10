238fb8f2c7b3c1c29ad30749f01398fffd67da37
pri65 <priyankasoniii65@gmail.com>
2022-07-10 20:21:02 +0530

    new

--- unit4/day7/timer3/src/Components/TimerCounterWithCleanup.jsx
+++ unit4/day7/timer3/src/Components/TimerCounterWithCleanup.jsx
@@ -5,17 +5,17 @@ function TimerCounterWithCleanup() {
   const [count,setCount]=useState(0);
 
     useEffect(() => {
-      setInterval(() => {
-        console.log(`closure:value is ${count}`);
-        setCount((prev) => prev+1);
-      },1000);
-    },[]);
-  return (
-    <div>
-      <h1>Counter:{count}</h1>
-        <h3>Update every second</h3>
-    </div>
-  )
+        console.log(`useEffect first`);
+        const id=setInterval(() => {
+            console.log(`setInterval`, Date.now());
+            setCount((prev) => prev+1);
+        },1000);
+      
+        const cleanup = () => {
+            clearInterval(id);
+        };
+        return cleanup;
+    },[])
 }
 
 export default TimerCounterWithCleanup;
