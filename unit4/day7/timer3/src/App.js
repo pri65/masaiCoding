4f2cbbb76196e0a8baed17d91220e82c7a266890
pri65 <priyankasoniii65@gmail.com>
2022-07-10 20:36:38 +0530

    new

--- unit4/day7/timer3/src/App.js
+++ unit4/day7/timer3/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import TimerCounterWithCleanup from './Components/TimerCounterWithCleanup';
 
 function App() {
+  const [show,setShow]=useState(true);
   return (
     <div className="App">
       <TimerCounterWithCleanup />
+      {show && <TimerCounterWithCleanup />}
+      <button onClick={() => setShow(!show)}>TOGGLE</button>
     </div>
   );
 }
