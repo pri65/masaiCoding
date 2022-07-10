1eaa98e6fccaae06f99502e04e6dc1f5feaa5328
pri65 <priyankasoniii65@gmail.com>
2022-07-10 17:27:45 +0530

    new

--- unit4/day7/timer1/src/App.js
+++ unit4/day7/timer1/src/App.js
@@ -3,28 +3,16 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 import TimerCounter from './Components/TimerCounter';
+import TimerCounterWithCleanup from './Components/TimerCounterWithCleanup';
 import TimerCountSetTimeout from './Components/TimerCountSetTimeout';
 
 
 function App() {
-  // const [count,setCount] = useState(0);
-
-  // const startTimer = () => {
-  //   //setInterval(callback,timeDelay)
-  //   //it is a global scope
-  //   setInterval(() => {
-  //     console.log(`value is ${count}`)
-  //     setCount((prev) => prev+1);
-  //   },1000);
-  // };
-
-  // useEffect(() => {
-  //   startTimer();
-  // },[count]);
   return (
     <div className="App">
       {/* <TimerCountSetTimeout /> */}
-      <TimerCounter />
+      {/* <TimerCounter /> */}
+      <TimerCounterWithCleanup />
     </div>
   );
 }
