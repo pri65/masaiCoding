935dff74048d5cbca7bfc633175e1096282f5c46
pri65 <priyankasoniii65@gmail.com>
2022-07-12 14:57:48 +0530

    new

--- unit4/day8useRef/forms/src/App.js
+++ unit4/day8useRef/forms/src/App.js
@@ -1,10 +1,29 @@
 
 import './App.css';
 
+
+const initState = {
+  name:"",
+  gender:"",
+  role:"",
+  maritalStatus:""
+};
+
+
 function App() {
+  const [data,setData]=React.useState(initState);
+
+    const handleChange = (e) => {
+      const{name,value} =e.target;
+      setData({...data,[name]:value});
+    };
+    const {name,gender,role,maritalStatus} = data;
   return (
     <div className="App">
-      
+      <h1>Form</h1>
+      <input value={name} name="name" onChange={handleChange} />
+      <br />
+      <button onClick={() => console.log(data)}>CLICK</button>
     </div>
   );
 }
