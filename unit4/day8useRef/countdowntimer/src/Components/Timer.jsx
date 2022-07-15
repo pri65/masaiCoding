68d889c71f452cb0b704cb532321cd2238e0c035
pri65 <priyankasoniii65@gmail.com>
2022-07-15 14:06:37 +0530

    new

--- unit4/day8useRef/countdowntimer/src/Components/Timer.jsx
+++ unit4/day8useRef/countdowntimer/src/Components/Timer.jsx
@@ -1,26 +1,38 @@
-import React, {useRef,useEffect} from "react";
+import React, {useState,useEffect} from "react";
 
 
 function Timer() {
-    const value=useRef(0);
-    
-  
-        const handleStart = () => {
-            useEffect(() => {
-                    (value+1);
-                }, [value])
+    const [value, setValue] = useState(0)
+    const [stop, setStop] = useState(false)
+    useEffect(() => {
+        if(!stop && value > 0 ) {
+            setTimeout(() => {
+                setValue(value+1)
+            },1000)
         }
-    
-    
-    
+    }, [value, stop])
+    const handleStart =()=> {
+       setTimeout(() => {
+        setValue(value+1)
+       },1000)
+       setStop(false)
+    }
+    const handlePause = () => {
+        setStop(true)
+    }
+    const handleReset = () =>{
+        setValue(0)
+        setStop(true)
+    }
   return (
     <div>
-        {value}
-        <button onClick={handleStart}>START</button>
-        <button onClick={handlePause}>PAUSE</button>
-        {/* <button onClick={handleReset}>RESET</button> */}
+        <h3>Here is the Value : {value}</h3>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
+
 
 export default Timer
