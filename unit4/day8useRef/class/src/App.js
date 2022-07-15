b4530baa1f6bd6a68f681ca279b6a83eb00e22eb
pri65 <priyankasoniii65@gmail.com>
2022-07-15 14:21:05 +0530

    new

--- unit4/day8useRef/class/src/App.js
+++ unit4/day8useRef/class/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <button>FETCH DATA</button>
     </div>
   );
 }
