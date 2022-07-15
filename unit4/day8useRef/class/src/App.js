d8dbc856e34967d6b4a26a6906816682f44f0496
pri65 <priyankasoniii65@gmail.com>
2022-07-15 14:23:23 +0530

    new

--- unit4/day8useRef/class/src/App.js
+++ unit4/day8useRef/class/src/App.js
@@ -4,6 +4,26 @@ import './App.css';
 function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
+  
+  const fetchData = () => {
+
+  }
+
+  if(isLoading) {
+    return (
+      <div className='App'>
+        <h1>...Loading</h1>
+      </div>
+    )
+  }
+  if(isError) {
+    return (
+      <div className='App'>
+        <h1>Something Went Wrong</h1>
+      </div>
+    )
+  }
+  
   return (
     <div className="App">
       <h1>Hello</h1>
