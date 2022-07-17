175e6af5f3eb9c44c0fcf91eb6a874cea522aef2
pri65 <priyankasoniii65@gmail.com>
2022-07-17 17:28:55 +0530

    new

--- unit4/day8useRef/reactforms/src/App.js
+++ unit4/day8useRef/reactforms/src/App.js
@@ -21,6 +21,28 @@ return (
         <label>Name
           <input type="text" name="name" value={name} />
         </label>
+        <br />
+        <br />
+        <label>Email
+          <input type="email" name="email" value={email} />
+        </label>
+        <br />
+        <br />
+        <label>Married
+          <input type="checkbox" name="isMarried" value={isMarried} />
+        </label>
+        <br />
+        <br />
+        <select>
+          <option value="M">M</option>
+          <option value="F">F</option>
+          <option value="F">Do not want to disclose</option>
+        </select>
+        <br />
+        <br />
+        <label>Name
+          <input type="text" name="name" value={name} />
+        </label>
       </form>
     </div>
   );
