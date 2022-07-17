e2334f9a9e8b77cd741d772f576e43ea210a69f9
pri65 <priyankasoniii65@gmail.com>
2022-07-17 14:53:54 +0530

    new

---
+++ unit4/day8useRef/imagepreview/src/App.js
@@ -0,0 +1,21 @@
+import React, {useRef,useEffect} from "react";
+import './App.css';
+
+function App() {
+  const ref=React.useRef;
+
+  React.useEffect(() => {}, []);
+
+  const onSubmit = () => {
+    console.log(ref.current.files[0]);
+  }
+  return (
+    <div ref={ref} className="App">
+      <h1>Images</h1>
+      <input type="file" ref={ref} />
+      <button onClick={onSubmit}>SUBMIT</button>
+    </div>
+  );
+}
+
+export default App;
