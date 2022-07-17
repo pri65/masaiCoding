8f420262950c49181514edfbaff068aec2b9db49
pri65 <priyankasoniii65@gmail.com>
2022-07-17 15:10:45 +0530

    new

--- unit4/day8useRef/reactforms/src/App.js
+++ unit4/day8useRef/reactforms/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+
+const initState={
+  name:"",
+  isMarried:"",
+  country:"",
+  gender:"",
+  email:""
+}
+
 function App() {
-  return (
+const [formData, setFormData] = useState(initState);
+ const {name,isMarried,country,gender,email}=formData; 
+
+return (
     <div className="App">
-      
+      <h1>Form:Persons Details</h1>
+      <form onSubmit={() => {}}>
+        <label>Name
+          <input type="text" name="name" value={name} />
+        </label>
+      </form>
     </div>
   );
 }
