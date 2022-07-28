da9179735eee81446d30082f5c689bcdc98e32f5
pri65 <priyankasoniii65@gmail.com>
2022-07-28 16:39:47 +0530

    new

--- projects/restaurant_app/src/App.js
+++ projects/restaurant_app/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import UseEffect from "./component/Hooks/UseEffect";
+// import UseEffect from "./component/Hooks/UseEffect";
+import UseReducer from "./component/Hooks/UseReducer";
 // import UseState from "./component/Hooks/UseState";
 
 const App = () => {
-  return <UseEffect />
+  return <UseReducer />
 
 };
 
