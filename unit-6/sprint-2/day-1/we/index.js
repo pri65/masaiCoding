2bdd92f30daf76f8da3c46c38695230bd66436bd
pri65 <priyankasoniii65@gmail.com>
2022-10-16 13:35:24 +0530

    new

---
+++ unit-6/sprint-2/day-1/we/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+
+const app = express()
+
+
+//function -> which has access to request object, response object and next() 
+app.use((req,res,next) => {
+    console.log("a")
+    next() //after this execution is done
+    console.log("z")
+})
+
+app.get("/", (req,res) => {
+    res.send("welcome")
+})
+
+app.get("/about", (req,res) => {
+    res.send("welcome to about page")
+})
+
+app.listen(8080, () => {
+    console.log("app is running")
+})
